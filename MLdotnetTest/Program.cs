2a8240ea0608e6ac9321f03e4666cfb168952938
Benjamin Nims <bennims93@gmail.com>
2023-01-01 19:23:13 -0600

    Added the basic model.

--- MLdotnetTest/Program.cs
+++ MLdotnetTest/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using MLdotnetTest;
+using SentimentAnalysis;
+using static Microsoft.ML.DataOperationsCatalog;
+
+
+MLContext mlContext = new MLContext();
+
+string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+TrainTestData splitDataView = LoadData(mlContext);
+
+TrainTestData LoadData(MLContext mlContext)
+{
+    IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+    TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+    return splitDataView;
+}
+
+ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+{
+    var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText)).Append
+            (mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+    Console.WriteLine("=============== Create and Train the Model ===============");
+    var model = estimator.Fit(splitTrainSet);
+    Console.WriteLine("=============== End of training ===============");
+    Console.WriteLine();
+    return model;
+}
+
+ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+
+void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+{
+    Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+    IDataView predictions = model.Transform(splitTestSet);
+    CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+}
+
+Evaluate(mlContext, model, splitDataView.TestSet);
+
 
--- MLdotnetTest/SentimentData.cs
+++ MLdotnetTest/SentimentData.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using Microsoft.ML.Data;
+
+
 namespace MLdotnetTest
 {
-	public class SentimentData
-	{
-		public SentimentData()
-		{
-		}
-	}
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText;
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment;
+    }
+
+    public class SentimentPrediction : SentimentData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
 }
 
